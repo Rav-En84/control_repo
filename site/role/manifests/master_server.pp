e10c63dba845fe4cfa4da2e45bd2ae7a1dccafe4
Rav.En <39793191+Rav-En84@users.noreply.github.com>
2021-02-09 19:09:44 +0100

    Update master_server.pp

--- site/role/manifests/master_server.pp
+++ site/role/manifests/master_server.pp
@@ -1,7 +1,5 @@
 class role::master_server {
   include profile::base
-  #include profile::agent_nodes
-    docker::image { 'web.puppet.vm':
-    ensure => 'absent'
+  include profile::agent_nodes
   }
 }
